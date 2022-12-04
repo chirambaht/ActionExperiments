bc1c3a455ebd7c891e42050491d5c3a543897e89
Humphrey <chirambaht@gmail.com>
2022-12-04 17:24:54 +0200

    Removed fluff

--- collect_data.cpp
+++ collect_data.cpp
@@ -50,7 +50,7 @@ float		ypr[3];	  // [yaw, pitch, roll]   yaw/pitch/roll container and gravity ve
 int	 LED_RED = 2, LED_GREEN = 0, BUTTON = 3;
 bool state = 0;
 
-FILE *arq_Accel, *arq_Gyro, *arq_Quaternions, *arq_Euler, *arq_YawPitchRoll, *arq_LinearAcc, *arq_WorldAcc;
+FILE *arq_Accel, *arq_Gyro, *arq_Quaternions, *arq_All;
 
 std::string namepaste = "";
 
@@ -102,9 +102,6 @@ void setup() {
 		printf( "Enabling DMP...\n" );
 		mpu.setDMPEnabled( true );
 
-		// enable Arduino interrupt detection
-		// Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-		// attachInterrupt(0, dmpDataReady, RISING);
 		mpuIntStatus = mpu.getIntStatus();
 
 		// set our DMP Ready flag so the main loop() function knows it's okay to use it
@@ -114,10 +111,6 @@ void setup() {
 		// get expected DMP packet size for later comparison
 		packetSize = mpu.dmpGetFIFOPacketSize();
 	} else {
-		// ERROR!
-		// 1 = initial memory load failed
-		// 2 = DMP configuration updates failed
-		// (if it's going to break, usually the code will be 1)
 		printf( "DMP Initialization failed (code %d)\n", devStatus );
 	}
 
@@ -146,27 +139,19 @@ void loop() {
 			secondsb   = endb.tv_sec - startb.tv_sec;
 			usecondsb  = endb.tv_usec - startb.tv_usec;
 			timestartb = ( ( secondsb ) *1000 + usecondsb / 1000.0 ) + 0.5;
-
-			if( timestartb > 5000 && state != 1 ) {
-				execl( "/usr/bin/sudo", "sudo", "shutdown", "-h", "now", NULL );
-				return;
-			}
 		}
 
 		if( state ) {
 			fclose( arq_Accel );
 			fclose( arq_Gyro );
 			fclose( arq_Quaternions );
-			// fclose(arq_Euler);
-			// fclose(arq_YawPitchRoll);
-			fclose( arq_LinearAcc );
-			// fclose(arq_WorldAcc);
+			fclose( arq_All );
 			digitalWrite( LED_RED, HIGH );
 		} else {
 			digitalWrite( LED_RED, LOW );
 			DIR			*d;
 			struct dirent *dir;
-			d			= opendir( "/home/pi/MPU6050-Pi-Demo/Datas" );
+			d			= opendir( "Datas" );
 			int dir_len = 0;
 			if( d != NULL ) {
 				while( ( dir = readdir( d ) ) != NULL )
@@ -177,7 +162,7 @@ void loop() {
 			}
 
 			printf( "%d", dir_len );
-			namepaste = "/home/pi/MPU6050-Pi-Demo/Datas/data_" + std::to_string( dir_len - 2 );
+			namepaste = "Datas/data_" + std::to_string( dir_len - 2 );
 			printf( namepaste.c_str() );
 
 			std::string new_dir		 = namepaste;
@@ -188,10 +173,7 @@ void loop() {
 
 			std::string Data_Accel = namepaste + "/Data_Accel.txt", Data_Gyro = namepaste + "/Data_Gyro.txt",
 						Data_Quaternions = namepaste + "/Data_Quaternions.txt",
-						// Data_Euler= namepaste + "/Data_Euler.txt",
-						// Data_YawPitchRoll= namepaste + "/Data_YawPitchRoll.txt",
-				Data_LinearAcc = namepaste + "/Data_LinearAcc.txt";
-			// Data_WorldAcc = namepaste + "/Data_WorldAcc.txt";
+						Data_LinearAcc	 = namepaste + "/Data_LinearAcc.txt";
 
 			arq_Accel = fopen( Data_Accel.c_str(), "wt" );
 			fprintf( arq_Accel, "time,accx,accy,accz\n" );
@@ -201,18 +183,6 @@ void loop() {
 
 			arq_Quaternions = fopen( Data_Quaternions.c_str(), "wt" );
 			fprintf( arq_Quaternions, "time,qw,qx,qy,qz\n" );
-
-			// arq_Euler = fopen(Data_Euler.c_str(),"wt");
-			// fprintf(arq_Euler,"time,alfa,beta,gama\n");
-
-			// arq_YawPitchRoll = fopen(Data_YawPitchRoll.c_str(),"wt");
-			// fprintf(arq_YawPitchRoll,"time,yaw,pitch,roll\n");
-
-			arq_LinearAcc = fopen( Data_LinearAcc.c_str(), "wt" );
-			fprintf( arq_LinearAcc, "time,accx,accy,accz\n" );
-
-			// arq_WorldAcc = fopen(Data_WorldAcc.c_str(),"wt");
-			// fprintf(arq_WorldAcc,"time,accx,accy,accz\n");
 
 			gettimeofday( &startc, NULL );
 		}
@@ -229,7 +199,6 @@ void loop() {
 	if( fifoCount == 1024 ) {
 		// reset so we can continue cleanly
 		mpu.resetFIFO();
-		printf( "FIFO overflow!\n" );
 
 		// otherwise, check for DMP data ready interrupt (this should happen frequently)
 	} else if( fifoCount >= 42 ) {
@@ -243,65 +212,23 @@ void loop() {
 		useconds = endc.tv_usec - startc.tv_usec;
 		mtime	 = ( ( seconds ) *1000 + useconds / 1000.0 ) + 0.5;
 		// display time in milliseconds
-		printf( "\ntime %ld ms    ", mtime );
 
-#ifdef OUTPUT_READABLE_ACCEL
-		// display accel values in easy matrix form: x y z
 		mpu.dmpGetAccel( &acc, fifoBuffer );
-		printf( "accel  %6d %6d %6d    ", acc.x, acc.y, acc.z );
-#endif
-
-#ifdef OUTPUT_READABLE_GYRO
-		// display accel values in easy matrix form: x y z
 		mpu.dmpGetGyro( &gyr, fifoBuffer );
-		printf( "gryro  %6d %6d %6d    ", gyr.x, gyr.y, gyr.z );
-#endif
-
-#ifdef OUTPUT_READABLE_QUATERNION
-		// display quaternion values in easy matrix form: w x y z
 		mpu.dmpGetQuaternion( &q, fifoBuffer );
-		printf( "quat %7.5f %7.5f %7.5f %7.5f    ", q.w, q.x, q.y, q.z );
-		//  save quaternion values
-#endif
 
-#ifdef OUTPUT_READABLE_REALACCEL
-		// display real acceleration, adjusted to remove gravity
-		mpu.dmpGetQuaternion( &q, fifoBuffer );
-		mpu.dmpGetAccel( &acc, fifoBuffer );
-		mpu.dmpGetGravity( &gravity, &q );
-		mpu.dmpGetLinearAccel( &accReal, &acc, &gravity );
-		// printf("areal %6d %6d %6d    ", accReal.x, accReal.y, accReal.z);
-		//  save real acceleration
-		// fprintf(arq_LinearAcc,"%ld,%6d,%6d,%6d\n", mtime, aaReal.x, aaReal.y, aaReal.z);
-#endif
-
-#ifdef OUTPUT_READABLE_WORLDACCEL
-		// display initial world-frame acceleration, adjusted to remove gravity
-		// and rotated based on known orientation from quaternion
-		mpu.dmpGetQuaternion( &q, fifoBuffer );
-		mpu.dmpGetAccel( &acc, fifoBuffer );
-		mpu.dmpGetGravity( &gravity, &q );
-		mpu.dmpGetLinearAccelInWorld( &accWorld, &accReal, &q );
-		printf( "world  %6d %6d %6d     ", accWorld.x, accWorld.y, accWorld.z );
-		// save initial word-frame acceleration
-		// fprintf(arq_WorldAcc,"%ld,%7.2f,%7.2f,%7.2f\n", mtime, (double) aaWorld.x/4096, (double) aaWorld.y/4096,
-		// (double) (aaWorld.z-4096)/4096);
-#endif
-
-		printf( "\n" );
 		if( state ) {
 			fprintf( arq_Accel, "%ld,%6d,%6d,%6d\n", mtime, acc.x, acc.y, acc.z );
 			fprintf( arq_Gyro, "%ld,%6d,%6d,%6d\n", mtime, gyr.x, gyr.y, gyr.z );
 			fprintf( arq_Quaternions, "%ld,%7.5f,%7.5f,%7.5f,%7.5f\n", mtime, q.w, q.x, q.y, q.z );
-			fprintf( arq_LinearAcc, "%ld,%6d,%6d,%6d\n", mtime, accReal.x, accReal.y, accReal.z );
-			fprintf( arq_WorldAcc, "%ld,%6d,%6d,%6d\n", mtime, accWorld.x, accWorld.y, accWorld.z );
+			fprintf( arq_All, "%ld,%6d,%6d,%6d,%6d,%6d,%6d,%7.5f,%7.5f,%7.5f,%7.5f\n", mtime, acc.x, acc.y, acc.z,
+				gyr.x, gyr.y, gyr.z, q.w, q.x, q.y, q.z );
 		}
 	}
 }
 
 int main() {
 	setup();
-	// usleep(100000);
 	while( 1 ) {
 		loop();
 	}
